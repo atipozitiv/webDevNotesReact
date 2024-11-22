faab5493a633720a815fc094aaaae7f4c199689e
atipozitiv <timofejivanov7322@gmail.com>
2024-11-22 22:49:53 +0700

    start from begin
    sadly((

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import { useEffect, useState } from 'react'
-import noTasks from "./noTasks.jsx"
-import onOpen from './onOpen.jsx'
-import drawTasks from './drawTasks.jsx'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  onOpen();
-  noTasks();
-  let activeTask;
 
   return (
     <>
@@ -19,39 +13,7 @@ function App() {
         </div>
         <button onClick={() => mainButton()}>+</button>
       </div>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
-  )
-
-  const [todo, setTodo] = useEffect([]);
-  return (
-    <button onClick={() => {setTodo([...todo, "1"])}}>+</button>
-  ){todo.map((todo, index) => {
-      return (
-        <>
-          <h1 key={index}>{todo}</h1>
-        </>
-      )
-    })}
   )
 }
 
--- src/deleteSpecButtons.jsx
+++
@@ -1,8 +0,0 @@
-function deleteSpecButtons() {
-  const elements = document.getElementsByClassName("spec-buttons");
-  while(elements.length > 0) {
-      elements[0].parentNode.removeChild(elements[0]);
-  }   
-}
-
-export default deleteSpecButtons
